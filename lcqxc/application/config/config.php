b57de6b5a4e08b8427e10f07689a6e189cab58b2
ptangentalex <ptangentalex@gmail.com>
2015-06-13 12:27:01 +0800

    m_index reference

--- lcqxc/application/config/config.php
+++ lcqxc/application/config/config.php
@@ -211,7 +211,7 @@ $config['directory_trigger'] = 'd';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
@@ -498,3 +498,8 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+/*
+ * datezone
+ */
+ini_set('date.timezone','Asia/shanghai');
